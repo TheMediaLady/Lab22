46ee0be764ff67e9e220f49aecc3c4666b7f93ba
TheMediaLady <themedialady@blairunlimited.com>
2018-08-29 14:30:56 -0400

    first commit

---
+++ Models/NewUserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class NewUserModel
+    {
+        public string LastName { get; set;}
+        public string FirstName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public NewUserModel(string firstName)
+        {
+            FirstName = firstName;
+        }
+        public string NewUserThanks()
+        {
+            return ($"Thank you {FirstName} for registering");
+        }
+    }
+
+    
+}
---
+++ Models/PasswordModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class PasswordModel
+    {
+        public string Password;
+
+        public PasswordModel(string password)
+        {
+            Password = password;
+        }
+       
+        public string ValidatePassword()
+            
+        {
+            return ValidatePassword();
+        }
+        
+    }
+    
+   
+}
